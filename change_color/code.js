25b0b94e2a9b43f948f91aeb5e4e9c0da7c688fb
souldeveloper <souldeveloper@gmail.com>
2021-04-23 17:19:35 +0200

    claro oscuro

---
+++ change_color/code.js
@@ -0,0 +1,13 @@
+/* seleccion elementos del dom */
+const check = document.querySelector('.toggle')
+const body = document.querySelector('.body')
+
+check.addEventListener('click', function(){
+    
+    if(this.checked){
+        body.classList.add('change-color')
+    }else{
+        body.classList.remove('change-color')
+    }
+})
+
