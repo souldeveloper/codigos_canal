d4bd16fa23b26837765c76b46e6633b69762990e
souldeveloper <souldeveloper@gmail.com>
2021-06-04 14:19:17 +0200

    agenda_mysql

---
+++ agenda_mysql/src/mysql_conector.js
@@ -0,0 +1,48 @@
+//importar mysql
+import mysql from 'mysql'
+let todos
+//crear la conexion
+const conector = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'manuel',
+        password: '123456',
+        database: 'agenda_contactos'
+    }
+)
+
+const conectar = () => {
+    conector.connect(err => {
+        if(err) throw err
+        console.log('conectado')
+    })
+}
+
+const agregarContacto = (numero, nombre) => {
+    const sql = `INSERT INTO agenda (id_contacto, numero_contacto, nombre_contacto) VALUES (${null}, ${numero}, "${nombre}")`
+    conector.query(sql, function(err, result, filed){
+        if(err) throw err
+        console.log(result)
+    })
+}
+
+const obtenerContactos = ()=> {
+    const sql = 'SELECT * FROM agenda'
+    conector.query(sql, function(err, result, field){
+        todos = result
+    })
+    return todos
+}
+
+const borrarContacto = id => {
+    const sql = `DELETE FROM agenda where id_contacto=${id}`
+    conector.query(sql)
+}
+
+
+
+
+
+
+
+export {agregarContacto, obtenerContactos, borrarContacto}
