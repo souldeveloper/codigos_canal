53f5c3a703063848d2e4055415f62f98c81314ce
Manuel <souldeveloper@gmail.com>
2022-02-11 14:31:56 +0100

    slider en react

---
+++ slider/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Slider from './componets/Slider';
+function App() {
+  return (
+    <div className="App">
+      <Slider ></Slider>
+      <Slider></Slider>
+    </div>
+  );
+}
+
+export default App;
